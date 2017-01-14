dab56c55dba44e15b8c8279d2e1af45b26cd6bb2
leo <leo493852107@163.com>
2017-01-14 16:37:14 +0800

    finish 6

---
+++ 6/vue-demo/src/components/News.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="news">
+		<h3>我是新闻</h3>
+		<ul>
+			<li v-for="(val,index) in news">
+				{{val}}
+			</li>
+		</ul>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'news',
+		data () {
+			return {
+				news: ['111','222','333','444']
+			}
+		}
+	}
+</script>
+<style>
+	li {
+		border-bottom: 1px dashed #dedede;
+	}
+</style>
